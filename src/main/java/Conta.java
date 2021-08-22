7b3e8484bdfa836ba145a541edbfd2b91722f956
Thiago Winvirg <thiagowinvirg@gmail.com>
2021-08-22 20:11:50 -0300

    concluido

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -3,104 +3,133 @@
  *
  */
 public class Conta {
-	
-	private int numero;
-	private double saldo;
-	private double limite = 100;
-	private double[] extrato;
-	private int operacoes = 10; // A conta suporta no maximo 10 operações
-	
-    public Conta(int numero, double saldo) {
-    	this.numero = numero;
-    	this.saldo = saldo;
+    private int numero;
+    private double saldo;
+    private double limitless;
+    private double limite;
+    private double[] extrato = new double[10];
+
+    public Conta(int numero, double saldoSemLimite) {
+        this.numero = numero;
+        this.limitless = saldoSemLimite;
+        this.limite = 100;
+        this.saldo = saldoSemLimite + limite;
+        System.out.println("Conta criada!!");
+        saldoLimite();  
     }
 
-    
     public int getNumero() {
-        return this.numero;
+        return numero;
     }
     
     public double getSaldo() {
-    	if(this.limite == 100) {
-    		return this.saldo+limite;
-    	}else
-        return saldo; 
+        return saldo;
     }
 
-    
     public double getLimite() {
         return limite;
     }
-    
-    public void setExtrato(double valor){
-        for(int i = 0; i >= extrato.length; i++){
-            extrato[i]=valor;
-        }
-    }
-    
-    public boolean sacar(double valor) {
-       if(valor<0||valor>saldo+limite||operacoes<1) {		//não pode sacar valor negativo; não pode sacar um valor maior que saldo+limite; teste se pode fazer operacoes
-    	   return false;
-       }else {
-    	   if(saldo>valor) {				//saque com saldo
-    		   saldo-=valor;
-    		   operacoes--;
-    		   setExtrato(valor);
-    	   }else {							//saque sem saldo usando limite;
-    		   double guardaSaldo = saldo;
-    		   saldo=(saldo+limite)-valor;
-    		   limite = saldo;
-    		   operacoes--;
-    		   setExtrato(valor);
-    	   }
-    	   return true;
-       }
-    }
 
+    public boolean sacar(double valor) {
+        if(valor > saldo || valor < 0.00){
+            System.out.println("Erro " + valor);
+            return false;
+        }else if(valor == saldo){
+            saldo -= valor;
+            limitless = 0.0;
+            limite = 0.0;
+            setExtrato(-valor);
+            System.out.println("Sacou " + valor);
+            saldoLimite();   
+            return true;
+        }else{
+            if(valor <= limitless){
+            	limitless -= valor;
+                saldo -= valor;
+                setExtrato(-valor);
+                System.out.println("Sacou " + valor);
+                saldoLimite();
+                return true;
+            }else{
+                limite -= valor - limitless;
+                limitless = 0.00;
+                saldo -= valor;
+                setExtrato(-valor);
+                System.out.println("Sacou " + valor);
+                saldoLimite();
+                return true;
+            }          
+        }
+
+    }
    
     public boolean depositar(double valor) {
-        if(valor<0||operacoes<1) {			//não pode sacar alor negativo nem fazer mais que 10 operacoes
-        	return false;
-        }else {
-        	if(limite!=100) {				//deposito com limite ultilizado
-	       		limite = 100;
-	        	saldo+=valor;
-		        operacoes--;
-		        setExtrato(valor);
-        	}else {							//deposito com limite intacto
-        		saldo+=valor;
-	        	operacoes--;
-	        	setExtrato(valor);
-        	}
-        	return true;
+        if(valor < 0.00){
+            System.out.println("Erro " + valor);
+            return false;
+        }else{
+            if(limite <= 100.00 && valor <= 100){
+                if(limite + valor >= 100.00){
+                	limitless += (limite + valor - 100.00);
+                    limite = 100.00;
+                    saldo = limite + limitless;
+                    setExtrato(valor);
+                    System.out.println("Depositou " + valor);
+                    saldoLimite();
+                    return true;    
+                }else{
+                    limite += valor;
+                    saldo = limite + limitless;
+                    System.out.println("Depositou " + valor);
+                    setExtrato(valor);
+                    saldoLimite();
+                    return true;
+                }
+            }else{
+            	limitless += valor;
+                saldo = limite + limitless;
+                setExtrato(valor);
+                System.out.println("Depositou " + valor);
+                saldoLimite();
+                return true;    
+            }
+            
         }
-    }
 
+    }
     
     public boolean transferir(Conta destino, double valor) {
-        if(valor<0||valor>saldo+limite||operacoes<1) {
-        	return false;
-        }else {
-        	if(valor>saldo) {
-        		saldo = (saldo+limite) - valor;
-        		operacoes--;
-        		setExtrato(valor);
-        	}else {
-        		saldo-=valor;
-        		destino.saldo+=valor;
-        		operacoes--;
-        		setExtrato(valor);
-        	}
-        	return true;
+        if(valor < 0.00 || valor > getSaldo()){
+            System.out.println("Erro " + valor);
+            return false;
+        }else{
+            sacar(valor);
+            destino.depositar(valor);
+            System.out.println("transferiu " + valor);
+            saldoLimite();
+            return true;
         }
     }
 
-
     public double[] verExtrato() {
-    	double[] verExtrato = new double[10];
-        for (int i = 0; i <= extrato.length; i++) {
-            verExtrato[i] = extrato[i];
+        double[] verExtrato = new double[10] ;
+        for (int i = 0; i < extrato.length; i++) {
+            if(extrato[i] != 0) verExtrato[i] = extrato[i];
         }
         return verExtrato;
     }
+
+    public void setExtrato(double valor){
+        for (int i = 0; i < extrato.length; i++) {
+            if(extrato[i] == 0 ){
+                extrato[i] = valor;
+                break;
+            }
+        }
+    }
+
+    public void saldoLimite(){
+        System.out.println("Saldo: " + getSaldo() + ", Limite: " + getLimite());
+    }
+
 }
